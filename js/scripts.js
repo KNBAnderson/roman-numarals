12e76f7484e6e9600df2d8127ff22dc3bf2f4255
Katlin Anderson and Nathan Aden <katlin@>
2019-04-04 09:27:27 -0700

    add events for inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+
 $(document).ready(function (){
 
+  $('#arabic').on('input',function(e){
+    var number = ('#arabic').val();
+
+    if(isNaN($('#arabic').val())) {
+
+    }
+
+    $('#roman').val($('#arabic').val());
+  });
+
+  $('#roman').on('input',function(e){
+    $('#arabic').val($('#roman').val());
+  });
+
 });
+function convertToArabic() {
+
+}
+
+
+function convertToRoman () {
+
+}
